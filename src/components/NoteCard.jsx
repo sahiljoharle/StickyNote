15ffc1efb7182eaaddb1e362fc6774cace387916
sahiljoharle <sahiljoharle@gmail.com>
2024-12-03 16:37:30 -0800

    Modification on Note Card

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,10 +1,13 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import Trash from "../icons/Trash";
+import { setNewOffset } from "../utils";
 
 const NoteCard = ({ note }) => {
   const body = JSON.parse(note.body);
-  let position = JSON.parse(note.position);
+  let [position, setPosition] = useState(JSON.parse(note.position));
   const colors = JSON.parse(note.colors);
+  let mouseStartpos = { x: 0, y: 0 };
+  const cardRef = useRef(null);
 
   const textAreaRef = useRef(null);
 
@@ -18,8 +21,34 @@ const NoteCard = ({ note }) => {
     current.style.height = current.scrollHeight + "px";
   };
 
+  const mouseDown = (e) => {
+    mouseStartpos.x = e.clientX;
+    mouseStartpos.y = e.clientY;
+
+    document.addEventListener("mousemove", mouseMove);
+    document.addEventListener("mouseup", mouseUp);
+  };
+
+  const mouseMove = (e) => {
+    const mouseMoveDir = {
+      x: mouseStartpos.x - e.clientX,
+      y: mouseStartpos.y - e.clientY,
+    };
+    mouseStartpos.x = e.clientX;
+    mouseStartpos.y = e.clientY;
+
+    const newPosition = setNewOffset(cardRef.current, mouseMoveDir);
+    setPosition(newPosition);
+  };
+
+  const mouseUp = () => {
+    document.removeEventListener("mousemove", mouseMove);
+    document.removeEventListener("mouseup", mouseUp);
+  };
+
   return (
     <div
+      ref={cardRef}
       className="card"
       style={{
         backgroundColor: colors.colorBody,
@@ -27,7 +56,11 @@ const NoteCard = ({ note }) => {
         top: `${position.y}px`,
       }}
     >
-      <div className="card-header" style={{ background: colors.colorHeader }}>
+      <div
+        className="card-header"
+        onMouseDown={mouseDown}
+        style={{ background: colors.colorHeader }}
+      >
         <Trash />
       </div>
       <div className="card-body">
